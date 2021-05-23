68e99396a5cfa64b04b11ea6cc12dc0846870cf9
Abigail Sloan <abigail.uy.sloan@gmail.com>
2021-05-23 10:24:23 +0900

    Updated javascript and html, manipulating the DOM to play the game instead of through alerts and window.prompt. Need to update the CSS to create a better UI.

---
+++ scripts/javascript-original.js
@@ -0,0 +1,71 @@
+//randomly returns rock, paper, or scissors as computerPlay
+function computerPlay(items){
+    return items[Math.floor(Math.random()*items.length)];
+}
+let items = ["Rock", "Scissors", "Paper"];
+
+//accepts playerSelection parameters, returning a number value and displays a string with the
+//winner's info. Only accepts "rock, paper, scissors. Only plays one round
+function gameTime(playerSelection, computerSelection){
+    let lowerCase = playerSelection.toLowerCase();
+    let playerSelection2 = playerSelection.charAt(0).toUpperCase() + lowerCase.slice(1);
+    if (playerSelection2 == "Rock" && computerSelection == "Paper") {
+        alert("You lose this round! Paper beats Rock, you sucker!");
+        return -1;
+    } else if (playerSelection2 == "Rock" && computerSelection == "Scissors") {
+        alert("You win this round... Rock beats scissors. I'm gonna cry...");
+        return 1;
+    } else if (playerSelection2 == "Rock" && computerSelection == "Rock") {
+        alert("We tie this round. Rock can't beat Rock. Better luck next time maybe?");
+        return 0;
+    } else if (playerSelection2 == "Paper" && computerSelection == "Paper") {
+        alert("We tie this round. Paper can't beat Paper. Better luck next time maybe?");
+        return 0;
+    } else if (playerSelection2 == "Paper" && computerSelection == "Scissors") {
+        alert("You lose this round! Scissors beat Paper, you sucker!");
+        return -1;
+    } else if (playerSelection2 == "Paper" && computerSelection == "Rock") {
+        alert("You win this round... Paper beats Rock. I'm gonna cry...");
+        return 1;
+    } else if (playerSelection2 == "Scissors" && computerSelection == "Paper") {
+        alert("You win this round... Scissors beats Paper. I'm gonna cry...");
+        return 1;
+    } else if (playerSelection2 == "Scissors" && computerSelection == "Scissors") {
+        alert("We tie this round. Scissors can't beat Scissors. Better luck next time maybe?");
+        return 0;
+    } else if (playerSelection2 == "Scissors" && computerSelection == "Rock") {
+        alert("You lose this round! Rock beats Scissors, you sucker!");
+        return -1;
+    } else {
+        alert("Invalid. There was a misspelling. The overall winner will not be calculated.");
+        return -50;
+    }
+}
+
+//runs the game give times and counts points. Returns total points.
+function gameFiveTimes(){
+    point1 = gameTime(window.prompt("Rock, paper, or scissors?"), computerPlay(items));
+    point2 = gameTime(window.prompt("Rock, paper, or scissors?"), computerPlay(items));
+    point3 = gameTime(window.prompt("Rock, paper, or scissors?"), computerPlay(items));
+    point4 = gameTime(window.prompt("Rock, paper, or scissors?"), computerPlay(items));
+    point5 = gameTime(window.prompt("Rock, paper, or scissors?"), computerPlay(items));
+    return point1 + point2 + point3 + point4 + point5;;
+}
+
+//displays a message dependant on the total amount of points user received
+function whoIsTheWinner() {
+    if (total > 0) {
+        return "You win >:(";
+    } else if (total == 0) {
+        return "We tie... Try again?";
+    } else if (total < 0 && total >= -5) {
+        return "You LOSE! >:)";
+    } else {
+        return "invalid input received";
+    }
+}
+
+//main body 
+window.alert("This game plays five rounds. Best 3 out of 5 wins.");
+let total = gameFiveTimes();
+alert(whoIsTheWinner());
--- scripts/javascript.js
+++ scripts/javascript.js
@@ -1,71 +1,86 @@
-//randomly returns rock, paper, or scissors as computerPlay
-function computerPlay(items){
-    return items[Math.floor(Math.random()*items.length)];
+function computerPlay(choices){
+    return choices[Math.floor(Math.random()*choices.length)];
 }
-let items = ["Rock", "Scissors", "Paper"];
 
-//accepts playerSelection parameters, returning a number value and displays a string with the
-//winner's info. Only accepts "rock, paper, scissors. Only plays one round
-function gameTime(playerSelection, computerSelection){
-    let lowerCase = playerSelection.toLowerCase();
-    let playerSelection2 = playerSelection.charAt(0).toUpperCase() + lowerCase.slice(1);
-    if (playerSelection2 == "Rock" && computerSelection == "Paper") {
-        alert("You lose this round! Paper beats Rock, you sucker!");
-        return -1;
-    } else if (playerSelection2 == "Rock" && computerSelection == "Scissors") {
-        alert("You win this round... Rock beats scissors. I'm gonna cry...");
-        return 1;
-    } else if (playerSelection2 == "Rock" && computerSelection == "Rock") {
-        alert("We tie this round. Rock can't beat Rock. Better luck next time maybe?");
-        return 0;
-    } else if (playerSelection2 == "Paper" && computerSelection == "Paper") {
-        alert("We tie this round. Paper can't beat Paper. Better luck next time maybe?");
-        return 0;
-    } else if (playerSelection2 == "Paper" && computerSelection == "Scissors") {
-        alert("You lose this round! Scissors beat Paper, you sucker!");
-        return -1;
-    } else if (playerSelection2 == "Paper" && computerSelection == "Rock") {
-        alert("You win this round... Paper beats Rock. I'm gonna cry...");
-        return 1;
-    } else if (playerSelection2 == "Scissors" && computerSelection == "Paper") {
-        alert("You win this round... Scissors beats Paper. I'm gonna cry...");
-        return 1;
-    } else if (playerSelection2 == "Scissors" && computerSelection == "Scissors") {
-        alert("We tie this round. Scissors can't beat Scissors. Better luck next time maybe?");
-        return 0;
-    } else if (playerSelection2 == "Scissors" && computerSelection == "Rock") {
-        alert("You lose this round! Rock beats Scissors, you sucker!");
-        return -1;
+var threeChoices = ["Rock", "Paper", "Scissors"];
+
+var userPoints = 0;
+var computerPoints = 0;
+
+var buttons = document.querySelectorAll(".button");
+
+//event listener to take button clicks as player input for single round, then results are input into updateScore.
+
+for (var i = 0; i < 3; i++) {
+    buttons[i].addEventListener("click", function(){
+            if (this.classList.contains("rock")){ 
+                updateScore(singleRound("rock", computerPlay(threeChoices)));
+                winningMessage();
+                return;
+            } else if (this.classList.contains("paper")){
+                updateScore(singleRound("paper", computerPlay(threeChoices)));
+                winningMessage();
+                return;
+            } else {
+                updateScore(singleRound("scissors", computerPlay(threeChoices)));
+                winningMessage();
+                return;
+            }
+    });
+}
+
+//decides and alerts who wins the round. returns values to updateScore. Stops calculating after five points.
+function singleRound (playerSelection, computerSelection){
+    if (userPoints < 5 && computerPoints < 5) {
+        document.getElementsByClassName("computer input")[0].innerHTML = "The computer chose: " + computerSelection;
+            if (playerSelection == "rock" && computerSelection == "Paper") {
+                //alert("You lose this round. Paper beats Rock.");
+                return [0, 1]
+            } else if (playerSelection == "rock" && computerSelection == "Scissors") {
+                //alert("You win this round. Rock beats scissors.");
+                return [1, 0];
+            } else if (playerSelection == "rock" && computerSelection == "Rock") {
+                //alert("We tie this round. Rock doesn't beat Rock.");
+                return [0, 0];
+            } else if (playerSelection == "paper" && computerSelection == "Paper") {
+                //alert("We tie this round. Paper doesn't beat Paper.");
+                return [0, 0];
+            } else if (playerSelection == "paper" && computerSelection == "Scissors") {
+                //alert("You lose this round. Scissors beat Paper.");
+                return [0, 1];
+            } else if (playerSelection == "paper" && computerSelection == "Rock") {
+                //alert("You win this round. Paper beats Rock.");
+                return [1, 0];
+            } else if (playerSelection == "scissors" && computerSelection == "Paper") {
+                //alert("You win this round. Scissors beats Paper.");
+                return [1, 0];
+            } else if (playerSelection == "scissors" && computerSelection == "Scissors") {
+                //alert("We tie this round. Scissors doesn't beat Scissors.");
+                return [0, 0];
+            } else if (playerSelection == "scissors" && computerSelection == "Rock") {
+                //alert("You lose this round! Rock beats Scissors.");
+                return [0, 1]
+            }
+
+}
+}
+
+//allows the output of singleRound be tallied on screen
+function updateScore(arr){
+    document.getElementsByClassName("player results")[0].innerHTML = "Player Count = " + (userPoints += arr[0]);
+    document.getElementsByClassName("computer results")[0].innerHTML = "Computer Count = " + (computerPoints += arr[1]);
+}
+
+//displays who the winner is after five points
+function winningMessage(){
+    var displayWinner = document.getElementsByClassName("winning message")[0].innerHTML;
+    if (userPoints > computerPoints){
+        displayWinner.innerHTML = "You win!"
+        return;
+    } else if (computerPoints > userPoints){
+        displayWinner.innerHTML = "You lose!"
+        return;
     } else {
-        alert("Invalid. There was a misspelling. The overall winner will not be calculated.");
-        return -50;
+        return;
     }
 }
-
-//runs the game give times and counts points. Returns total points.
-function gameFiveTimes(){
-    point1 = gameTime(window.prompt("Rock, paper, or scissors?"), computerPlay(items));
-    point2 = gameTime(window.prompt("Rock, paper, or scissors?"), computerPlay(items));
-    point3 = gameTime(window.prompt("Rock, paper, or scissors?"), computerPlay(items));
-    point4 = gameTime(window.prompt("Rock, paper, or scissors?"), computerPlay(items));
-    point5 = gameTime(window.prompt("Rock, paper, or scissors?"), computerPlay(items));
-    return point1 + point2 + point3 + point4 + point5;;
-}
-
-//displays a message dependant on the total amount of points user received
-function whoIsTheWinner() {
-    if (total > 0) {
-        return "You win >:(";
-    } else if (total == 0) {
-        return "We tie... Try again?";
-    } else if (total < 0 && total >= -5) {
-        return "You LOSE! >:)";
-    } else {
-        return "invalid input received";
-    }
-}
-
-//main body 
-window.alert("This game plays five rounds. Best 3 out of 5 wins.");
-let total = gameFiveTimes();
-alert(whoIsTheWinner());
